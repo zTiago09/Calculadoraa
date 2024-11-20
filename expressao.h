bcddbd8b0f357af5f4107c9fe55f2881a4aee742
lomarkz <168549843+lomarkz@users.noreply.github.com>
2024-11-20 12:59:42 -0300

    Update expressao.h

--- expressao.h
+++ expressao.h
@@ -4,13 +4,13 @@
 
 typedef struct {
 
-    char posFixa[512];     // Expressão na forma pos fixa, como 3 12 4 + *
-    char inFixa[512];      // Expressão na forma in fixa, como 3 * (12 + 4)
-    float Valor;           // Valor numérico da expressão  
+    char posFixa[512];     
+    char inFixa[512];      
+    float Valor;          
 
 } Expressao;
 
-char *getFormaInFixa(char *Str);    // Retorna a forma inFixa de Str (posFixa)
-float getValor(char *Str);          // Calcula o valor de Str (na forma posFixa)
+char *getFormaInFixa(char *Str);    
+float getValor(char *Str);          
 
 #endif
